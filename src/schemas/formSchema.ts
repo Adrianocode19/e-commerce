f6849b1d418f171b871c79c7243a05bea6cf8a8c
Adriano Carlos Texeira <adrianoctl20@gmail.com>
2025-08-27 20:12:44 -0400

    feat: melhorando validação da data de nascimento

--- src/schemas/formSchema.ts
+++ src/schemas/formSchema.ts
@@ -1,5 +1,18 @@
 import { z } from 'zod'
 
+const isValidDate = (dateStr: string) => {
+  const [day, month, year] = dateStr.split('/').map(Number)
+  const date = new Date(year, month - 1, day)
+  const today = new Date()
+
+  return (
+    date.getFullYear() === year &&
+    date.getMonth() === month - 1 &&
+    date.getDate() === day &&
+    year <= today.getFullYear()
+  )
+}
+
 export const formSchema = z.object({
   name: z
     .string('O nome é obrigatório')
@@ -7,11 +20,11 @@ export const formSchema = z.object({
     .max(100, 'Não pode ter mais de 100 caracteres')
     .regex(/^[A-Za-zÀ-ÿ\s]+$/, 'O nome deve conter apenas letras'),
   email: z.string('O e-mail é obrigatório').email('Email inválido'),
-  phone: z.string('O telefone é obrigatório').min(15, 'No minimo 15 caracteres').max(15),
+  phone: z.string('O telefone é obrigatório').min(15, 'No mínimo 15 caracteres').max(15),
   birthdate: z
     .string('A data de nascimento é obrigatória')
-    .min(10, 'Data inválida')
-    .max(10, 'Data inválida'),
+    .regex(/^\d{2}\/\d{2}\/\d{4}$/, 'Formato deve ser dd/mm/yyyy')
+    .refine(isValidDate, 'Data de nascimento inválida'),
   message: z
     .string('A mensagem é obrigatória')
     .min(10, 'Mensagem muito curta')
